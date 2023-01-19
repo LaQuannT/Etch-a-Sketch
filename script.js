213b3aa4b519a610859c373a89d417b81722141b
LaQuannT <117905467+LaQuannT@users.noreply.github.com>
2023-01-19 22:58:34 +0000

    Added divs for grid & hover effect

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
-const container = document.querySelector('.container');
+// Have script wait to run until page is loaded
+document.addEventListener('DOMContentLoaded',function() {
 
-container.style.gridTemplateColumns = "repeat(16, 1fr)";
-container.style.gridTemplateRows = "repeat(16, 1fr)";
+    // Get node from DOM
+    let container = document.querySelector('.container');
 
+    // Create a 16x16 grid - css grid template
+    container.style.gridTemplateColumns = 'repeat(16, 1fr)';
+    container.style.gridTemplateRows ='repeat(16, 1fr)';
+
+    //create divs for grid
+    let numDivs = 16 * 16;
+    for (let i = 0; i < numDivs; i++) {
+        let div = document.createElement('div');
+        div.classList.add('grid-item');
+        container.appendChild(div);
+    }
+
+    //create hover effect changing background color
+    const boxes = document.querySelectorAll('.grid-item');
+    for (let i = 0; i < boxes.length; i++) {
+        boxes[i].addEventListener('mouseover', function() {
+            this.style.backgroundColor = 'yellow';
+        });
+    }
+});

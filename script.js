cc2cbdcd4b4dde2d8212ec0384fcd3c128f7160e
LaQuannT <117905467+LaQuannT@users.noreply.github.com>
2023-01-20 09:44:15 +0000

    Added a function to reset grid when a new size is chosen

--- script.js
+++ script.js
@@ -1,26 +1,61 @@
 // Have script wait to run until page is loaded
 document.addEventListener('DOMContentLoaded',function() {
 
-    // Get node from DOM
-    let container = document.querySelector('.container');
+    //default gride size
+    createGrid(16);
 
-    // Create a 16x16 grid - css grid template
-    container.style.gridTemplateColumns = 'repeat(16, 1fr)';
-    container.style.gridTemplateRows ='repeat(16, 1fr)';
+   // Call button node and add event listeners
+    const btn = document.querySelector('button');
+    btn.addEventListener('click', () => {
+        clearGrid();
 
-    //create divs for grid
-    let numDivs = 16 * 16;
-    for (let i = 0; i < numDivs; i++) {
-        let div = document.createElement('div');
-        div.classList.add('grid-item');
-        container.appendChild(div);
-    }
+    // Prompt user for input for size of grid
+   playersChoice = parseInt(prompt("Pick size of grid 1 - 100"));
 
-    //create hover effect changing background color
-    const boxes = document.querySelectorAll('.grid-item');
-    for (let i = 0; i < boxes.length; i++) {
-        boxes[i].addEventListener('mouseover', function() {
-            this.style.backgroundColor = 'yellow';
+   // Check that input is valid
+   if (playersChoice > 100 || playersChoice < 0) {
+    alert("Please select again");
+   } else {
+    createGrid(playersChoice);
+    draw();
+   }
+    
+});
+
+
+});
+
+function createGrid(size){
+     // Get node from DOM
+     let container = document.querySelector('.container');
+
+     // Create a 16x16 grid - css grid template
+     container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+     container.style.gridTemplateRows =`repeat(${size}, 1fr)`;
+ 
+     //create divs for grid
+     let numDivs = size * size;
+     for (let i = 0; i < numDivs; i++) {
+         let div = document.createElement('div');
+         div.classList.add('grid-item');
+         container.appendChild(div);
+     }
+}
+
+function draw() {
+ //create hover effect changing background color
+ const boxes = document.querySelectorAll('.grid-item');
+ for (let i = 0; i < boxes.length; i++) {
+    boxes[i].addEventListener('mouseover', function() {
+        this.style.backgroundColor = 'yellow';
         });
     }
-});
+}
+
+// Set grid background color to default color
+function clearGrid() {
+    let boxes = document.querySelectorAll('.grid-item');
+    boxes.forEach((box) => {
+        box.style.backgroundColor = 'lightgray';
+    });
+}
